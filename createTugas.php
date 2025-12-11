f1bc9fd8db20dc51578026901e7b56b7e76248b2
oMrPuPa <0rafipati0@gmail.com>
2025-12-11 16:18:00 +0700

    ada bug ghoib

---
+++ createTugas.php
@@ -0,0 +1,14 @@
+<?php
+include 'koneksi.php';
+$namaTugas = $_POST['namaTugas'];
+$deskripsi = $_POST['deskripsiTugas'];
+$tanggalDeadline = $_POST['tanggalDeadline'];
+$query = "INSERT INTO tugas (nama_tugas, deskripsi, tanggal_deadline) VALUES ('$namaTugas', '$deskripsi', '$tanggalDeadline')";
+
+if (mysqli_query(mysql: $koneksi, query: $query)) {
+    echo "Tugas berhasil ditambahkan";
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($koneksi);
+}
+mysqli_close(mysql: $koneksi);
+?>
