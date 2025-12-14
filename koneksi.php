2971cb6f0c0866d5819af6f65658b049e8034e65
Muhammad Rafi Putra Pati <0rafipati0@gmail.com>
2025-12-14 14:15:25 +0700

    bug
    padahal dari awal awal dah diubah tapi masih sama aja saat dipush

--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,7 @@ $host = 'localhost';
 $user = 'root';
 $pass = '';
 $namadb = 'ujian_pweb';
-$koneksi = mysqli_connect($host, $user, $pass, $namadb);
+$koneksi = mysqli_connect(hostname: $host,username: $user,password: $pass,database: $namadb);
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
